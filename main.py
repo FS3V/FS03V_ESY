b46266423a29464c8b1bcfee90dc830a2602d2df
hig <hig@hems.de>
2023-11-07 10:43:51 +0100

    diese lokale Datei wandert gleich auf den github...

---
+++ main.py
@@ -0,0 +1,13 @@
+def main():
+
+    print("Was möchten Sie tun?")
+    eingabe=input("Ihre Wahl: ")
+
+    while eingabe!="bye":
+            print("Was möchten Sie tun?")
+            eingabe=input("Ihre Wahl: ")
+            
+    print("Bye!")
+    
+if __name__ == "__main__":
+    main()
