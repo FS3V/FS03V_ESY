b7bac9856448b41601160b3932460f0373a45800
MXXX0R <max.kummer@hotmail.de>
2023-11-16 10:08:48 +0100

    added wurzel_n to menu selection

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 import square
 
 def main():
-    print("Was möchten Sie tun?")
+    print("Was möchten Sie 1tun?")
     print("[1]Quadratische Funktion loesen")
+    print("[2]Wurzel")
     eingabe=input("Ihre Wahl: ")
 
     while eingabe!="bye":
         if eingabe == "1":
             square()
+        if eingabe == "2":
+            wurzel_n()
         print("Was möchten Sie tun?")
         eingabe=input("Ihre Wahl: ")
-
-        if eingabe == "wurzel":
-            wurzel_n()
             
     print("Bye!")
     print("Farewell!")
