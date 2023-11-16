5846f48644e79860e3a59fc330d2ec8a9f305ca4
MXXX0R <max.kummer@hotmail.de>
2023-11-16 10:00:16 +0100

    Einrückung fix

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 import solve_square_function
 
 def main():
-
     print("Was möchten Sie tun?")
     print("[1]Quadratische Funktion loesen")
     eingabe=input("Ihre Wahl: ")
 
     while eingabe!="bye":
-            if eingabe == "1":
-                solve_square_function()
-            print("Was möchten Sie tun?")
-            eingabe=input("Ihre Wahl: ")
+        if eingabe == "1":
+            solve_square_function()
+        print("Was möchten Sie tun?")
+        eingabe=input("Ihre Wahl: ")
 
 	    if(eingabe == "wurzel"):
 			wurzel_n()
